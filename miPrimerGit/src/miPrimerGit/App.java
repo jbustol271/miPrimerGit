66369dc9d5560fdfa6d0ed5e092fef48ebd0b7ae
Javier Bustamante <jbustol271@g.educaand.es>
2026-03-23 10:44:01 +0100

    Cambiando el mensaje de salida. "Hola Mundo Git!!"

--- miPrimerGit/src/miPrimerGit/App.java
+++ miPrimerGit/src/miPrimerGit/App.java
@@ -13,7 +13,8 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo!!!");
+		// System.out.println("Hola Mundo!!!");
+		System.out.println("Hola Mundo Git!!!");
 
 	}
 
